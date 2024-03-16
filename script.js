2bc8e6a6f22c3e7dd2b2f4713e13f19a26de1358
mst4r1o <mst4r1o@gmail.com>
2024-03-16 22:25:47 +0800

    Add calculator algorithm
    There are seven cases to look out for:
    1. num operator num equal
    2. operator num equal
    3. num operator num operator num equal
    4. num operator num operator equal
    5. num operator num equal num ...
    6. num operator num equal operator num ...
    7. num operator num equal equal equal
    
    Allow the user to do normal calculations, inverse the sign of the first
    number, allow for multiple calculations before pressing equal, ensure if
    user press operator before equal that it will not take in effect, and
    allow user to work off the previous result etc..
    
    There are various checks and limitations implemented to ensure the code
    works for all seven cases, there should be better ways but this is the
    way I thought of myself.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Setting up basic math functions
+// Setting up basic math functions
 function addNum(a, b) {
     return a + b;
 };
@@ -15,51 +15,85 @@ function divideNum(a, b) {
     return a / b;
 };
 
-//Declaring variables for math equation to take place
+// Declaring variables for math equation to take place
 let prevNum = "0";
 let nextNum = "";
 let operator = null;
+let equalPressed = false;
 
 function operate(operator, a, b) {
     return operator(a, b);
 };
 
-//Setting up display
+// Setting up display
 const display = document.querySelector(".display");
 
-//Setting up number buttons
+// Setting up number buttons
 const numberBtns = document.querySelectorAll("button.number");
 numberBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
+        console.log(btn.name);
+        if (equalPressed) {
+            prevNum = "0";
+            nextNum = "";
+            operator = null;
+        }
         nextNum += btn.name;
         display.innerHTML = nextNum;
+        equalPressed = false;
     });
 });
 
-//Setting up operator buttons
+// Setting up operator buttons
 const opBtns = document.querySelectorAll("button.operator");
 opBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
+        console.log(btn.name, operator);
+
+        if (operator != null && !equalPressed) {
+            console.log("not null");
+            prevNum = operate(operator, +prevNum, +nextNum);
+        } else if (equalPressed) {
+            nextNum = "";
+        } else {
+            prevNum = nextNum !== "" ? nextNum : prevNum;
+        }
+
+        nextNum = "";
+        equalPressed = false;
+        display.innerHTML = prevNum;
+
         operator = btn.name == "add" ? addNum
                  : btn.name == "subtract" ? subtractNum
                  : btn.name == "multiply" ? multiplyNum
                  : btn.name == "divide" ? divideNum
                  : null;
-        console.log(operator);
     });
 });
 
 const equalBtn = document.querySelector("button.equal");
 equalBtn.addEventListener("click", () => {
-    prevNum = operate(operator, +prevNum, +nextNum);
+    console.log(equalBtn.name, operator);
+    if (operator == divideNum && nextNum == 0) {
+        clearBtn.click();
+        display.innerHTML = "Don't even.";
+        return;
+    }
+    
+    if (operator == null) {
+        prevNum = nextNum;
+    } else {
+        prevNum = operate(operator, +prevNum, +nextNum);
+    }
+    equalPressed = true;
     display.innerHTML = prevNum;
-    nextNum = ""; 
-})
+});
 
 const clearBtn = document.querySelector("button.clear");
 clearBtn.addEventListener("click", () => {
     display.innerHTML = 0;
-    prevNum = "";
+    prevNum = "0";
     nextNum = "";
     operator = null;
+    equalPressed = false;
 });
