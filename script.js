6dd6caa9f8692cdf5eecb4aac505ab0b59b9dd55
mst4r1o <mst4r1o@gmail.com>
2024-03-16 19:31:53 +0800

    Add buttons and event listeners to them
    Before figuring out the algorithm, add all the html buttons and also
    the event listeners that correspond to them. Also, created a display
    to display the number you key in or the result.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Setting up basic math functions
 function addNum(a, b) {
     return a + b;
 };
@@ -14,10 +15,51 @@ function divideNum(a, b) {
     return a / b;
 };
 
-let firstNum;
-let secondNum;
-let operator;
+//Declaring variables for math equation to take place
+let prevNum = "0";
+let nextNum = "";
+let operator = null;
 
 function operate(operator, a, b) {
     return operator(a, b);
-}
+};
+
+//Setting up display
+const display = document.querySelector(".display");
+
+//Setting up number buttons
+const numberBtns = document.querySelectorAll("button.number");
+numberBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        nextNum += btn.name;
+        display.innerHTML = nextNum;
+    });
+});
+
+//Setting up operator buttons
+const opBtns = document.querySelectorAll("button.operator");
+opBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        operator = btn.name == "add" ? addNum
+                 : btn.name == "subtract" ? subtractNum
+                 : btn.name == "multiply" ? multiplyNum
+                 : btn.name == "divide" ? divideNum
+                 : null;
+        console.log(operator);
+    });
+});
+
+const equalBtn = document.querySelector("button.equal");
+equalBtn.addEventListener("click", () => {
+    prevNum = operate(operator, +prevNum, +nextNum);
+    display.innerHTML = prevNum;
+    nextNum = ""; 
+})
+
+const clearBtn = document.querySelector("button.clear");
+clearBtn.addEventListener("click", () => {
+    display.innerHTML = 0;
+    prevNum = "";
+    nextNum = "";
+    operator = null;
+});
